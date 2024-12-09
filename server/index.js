35548088ecb5f0ed9aab4995126baa09a16ced47
me-abdullah <enxoemrick@gmail.com>
2024-12-09 16:48:37 +0500

    adding users

--- server/index.js
+++ server/index.js
@@ -1,5 +1,49 @@
 const express = require("express");
-const {ApolloServer} = require("@apollo/server");
-const {expressMiddleware} = require("@apollo/server/express4");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const {default:axios} = require("axios");
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+
+        typeDefs: `
+
+        type User{
+        id: ID!
+        name: String!
+        username: String!
+        email: String!
+        phone: String!
+        website: String!
+        }
+
+        type Todo{
+        id: ID!
+        title: String!
+        completed: Boolean
+        }
+
+        type Query{
+        getTodos: [Todo]
+         getAllUsers: [Users]
+        }
+        `,
+        resolvers: {
+            Query: {
+                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
+                getAllUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
+            }
+        },
+    });
+
+    app.use(bodyParser.json());
+    app.use(cors());
+    await server.start();
+    app.use("/graphql", expressMiddleware(server));
+
+    app.listen(8000, () => console.log(`Server Running on PORT 8000`));
+}
+
+startServer();
